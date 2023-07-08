a855da1255ece279ac4994bbdc63130ab1191671
adnanrangari <adnanrangari@gmail.com>
2023-07-09 02:27:20 +0530

    First commit by master branch

---
+++ calculator.java/src/calculator.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class calculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double num1, num2, result;
+
+        System.out.print("Enter the first number: ");
+        num1 = scanner.nextDouble();
+
+        System.out.print("Enter the second number: ");
+        num2 = scanner.nextDouble();
+
+        System.out.println("Select an operation: ");
+        System.out.println("1. Addition");
+        System.out.println("2. Subtraction");
+        System.out.println("3. Multiplication");
+        System.out.println("4. Division");
+
+        int operation = scanner.nextInt();
+
+        switch (operation) {
+            case 1:
+                result = num1 + num2;
+                System.out.println("The result of addition is: " + result);
+                break;
+
+            case 2:
+                result = num1 * num2;
+                System.out.println("The result of multiplication is: " + result);
+                break;
+            case 3:
+                if (num2 != 0) {
+                    result = num1 / num2;
+                    System.out.println("The result of division is: " + result);
+                } else {
+                    System.out.println("Cannot divide by zero.");
+                }
+                break;
+            default:
+                System.out.println("Invalid operation.");
+        }
+    }
+}
